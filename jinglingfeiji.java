ace6cf9f7d77143d310d3d0425b4cd5f1e8b40d8
yangyifeng <yangyifeng@idengyun.com>
2018-11-05 19:30:40 +0800

    jialei

--- jinglingfeiji.java
+++ jinglingfeiji.java
@@ -1,4 +1,4 @@
-精灵飞机只有我能打出来
+/*精灵飞机只有我能打出来
 我能针对
 你去了解下我们针对阵容
 随便针对，不怕
@@ -6,5 +6,11 @@ lgd咚咚咚
 
 再加一行
 
-无敌
-
+无敌*/
+class jinlin{
+	public static void main(String[] args) {
+		String str ="ll";
+		System.out.println(str);
+	}
+	
+}
