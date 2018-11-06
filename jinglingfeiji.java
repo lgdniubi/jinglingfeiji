693107e0c6d905caf2ba4e05dbcaa09c6476316e
yangyifeng <yangyifeng@idengyun.com>
2018-11-06 11:14:01 +0800

    xiugai
    Change-Id: Ied9f3dc38d85b947a2e1d98da0a4b0638018e542
    Signed-off-by: yangyifeng <yangyifeng@idengyun.com>

--- jinglingfeiji.java
+++ jinglingfeiji.java
@@ -9,8 +9,9 @@ lgd咚咚咚
 无敌*/
 class jinlin{
 	public static void main(String[] args) {
-		String str ="22";
-		System.out.println(str);
+		
+		String str2 ="23";
+		System.out.println(str2);
 	}
 	
 }
